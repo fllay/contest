f8a62c6bc2cc693739e9978b5d27f9f7d762b487
ThundluckS <thundluck.s@gmail.com>
2021-01-26 05:11:56 -0700

    Update check.py

--- check.py
+++ check.py
@@ -318,7 +318,7 @@ def main(argv):
         print(pred)
 
         # Initialize Performance Class
-        performance = Performance(pred,test)
+        performance = Performance(test,pred)
         
         # Print Statistical Summary
         #performance.summarize()
